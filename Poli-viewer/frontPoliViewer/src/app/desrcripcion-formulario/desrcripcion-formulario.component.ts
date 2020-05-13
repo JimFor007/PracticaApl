30485f12a323f3609b7c06351452c287f9d7610b
Jimmy Giovanny Forero Forero <jiforero2@poligran.edu.co>
2020-05-13 16:21:31 -0500

    documentation and better inscription form

--- Poli-viewer/frontPoliViewer/src/app/desrcripcion-formulario/desrcripcion-formulario.component.ts
+++ Poli-viewer/frontPoliViewer/src/app/desrcripcion-formulario/desrcripcion-formulario.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
-import { Event } from '../models/event.model';
 import { EventosService } from '../services/eventos.service'
 
 @Component({
@@ -26,12 +25,11 @@ export class DesrcripcionFormularioComponent implements OnInit {
   }
 
   inscripcion() {
-    console.log (this.rol);
-    /*this.event.numberParticipants += 1;
+    this.event.numberParticipants += 1;
     this.EventosService.updateEvent (this.event.idEvent, this.event).subscribe (
       res=>{
         console.log("******** EVENT UPDATED *******");
       },error => console.log(error)
-    );*/
+    );
   }
 }
